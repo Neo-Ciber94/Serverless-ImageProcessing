db4e186dbcd62f95c156a3125fe75f0d8a9c2d05
Freddy <neo-ciber_94@hotmail.com>
2023-07-01 23:25:52 -0400

    refactor

--- functions/image-processing/src/bin/get_image.rs
+++ functions/image-processing/src/bin/get_image.rs
@@ -1,8 +1,10 @@
 use image_processing::error::ResponseError;
-use lambda_http::{run, service_fn, Body, Error, IntoResponse, Request, Response};
+use lambda_http::{Body, Error, Request, Response};
 use reqwest::header;
 
 async fn function_handler(request: Request) -> Result<Response<Body>, Error> {
+    use lambda_http::IntoResponse;
+
     match image_processing::api::get_image_endpoint(request).await {
         Ok(mut res) => {
             let seconds = 60 * 60 * 24 * 365; // 1 year
@@ -20,6 +22,8 @@ async fn function_handler(request: Request) -> Result<Response<Body>, Error> {
 #[tokio::main]
 #[cfg(not(feature = "local"))]
 async fn main() -> Result<(), Error> {
+    use lambda_http::{run, service_fn};
+
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         .with_target(false)
@@ -32,54 +36,7 @@ async fn main() -> Result<(), Error> {
 #[tokio::main]
 #[cfg(feature = "local")]
 async fn main() {
-    use axum::{response::IntoResponse, Router};
-    use hyper::StatusCode;
-    use lambda_http::aws_lambda_events::query_map::QueryMap;
-    use lambda_http::RequestExt;
+    use hyper::Method;
 
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        .with_target(false)
-        .without_time()
-        .init();
-
-    async fn endpoint(
-        axum::extract::Query(query): axum::extract::Query<QueryMap>,
-        req: axum::http::Request<axum::body::Body>,
-    ) -> axum::response::Response {
-        let (parts, axum_body) = req.into_parts();
-        let bytes = match hyper::body::to_bytes(axum_body).await {
-            Ok(x) => x,
-            Err(err) => {
-                return (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()).into_response();
-            }
-        };
-
-        let request = Request::from_parts(parts, Body::Binary(bytes.to_vec()))
-            .with_query_string_parameters(query);
-
-        let res = match function_handler(request).await {
-            Ok(x) => x,
-            Err(err) => {
-                return (StatusCode::INTERNAL_SERVER_ERROR, err.to_string()).into_response();
-            }
-        };
-
-        let (parts, body) = res.into_parts();
-        let axum_body = axum::body::Body::from(body.to_vec());
-        axum::response::Response::from_parts(parts, axum::body::boxed(axum_body))
-    }
-
-    let app = Router::new().route("/api/image", axum::routing::get(endpoint));
-
-    let port: u16 = std::env::var("PORT")
-        .ok()
-        .and_then(|s| s.parse().ok())
-        .unwrap_or(5000);
-
-    let addr: std::net::SocketAddr = format!("0.0.0.0:{port}").parse().unwrap();
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    image_processing::utils::handle_local_request("/", Method::GET, function_handler).await
 }
--- functions/image-processing/src/lib.rs
+++ functions/image-processing/src/lib.rs
@@ -1,5 +1,8 @@
 pub mod api;
 pub mod error;
-
 mod process_image;
+
+#[cfg(feature = "local")]
+pub mod utils;
+
 pub use process_image::{process_image, ImageByteBuffer, ProcessingOptions};
