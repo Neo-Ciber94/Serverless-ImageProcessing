1510011a9a6bbcb7bbcc008e036e7233253ea8ce
Freddy <neo-ciber_94@hotmail.com>
2023-07-02 19:33:12 -0400

    Added api keys

--- lib/image-processing-stack.ts
+++ lib/image-processing-stack.ts
@@ -6,7 +6,7 @@ import * as path from 'path';
 import * as awsLogs from 'aws-cdk-lib/aws-logs';
 
 export class ImageProcessingStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, apiKeys: string[], props?: cdk.StackProps) {
     super(scope, id, props);
 
     const getImageHandler = new lambda.Function(this, "GetImage", {
@@ -26,7 +26,8 @@ export class ImageProcessingStack extends cdk.Stack {
       logRetention: awsLogs.RetentionDays.FIVE_DAYS,
       memorySize: 128,
       tracing: lambda.Tracing.ACTIVE,
-      timeout: cdk.Duration.minutes(3)
+      timeout: cdk.Duration.minutes(3),
+
     });
 
     const restApi = new apigateway.RestApi(this, "Api", {
@@ -35,10 +36,39 @@ export class ImageProcessingStack extends cdk.Stack {
       binaryMediaTypes: ["*/*"]
     });
 
+    const usagePlan = restApi.addUsagePlan("UsagePlan", {
+      quota: {
+        limit: 1000,
+        period: apigateway.Period.DAY
+      },
+      throttle: {
+        burstLimit: 10,
+        rateLimit: 5
+      },
+    });
+
+    if (apiKeys.length <= 0) {
+      throw new Error("Expected at least 1 api key");
+    }
+
+    for (let i = 0; i < apiKeys.length; i++) {
+      const apiKey = restApi.addApiKey(`DevApiKey-${i}`, {
+        apiKeyName: `DevApiKey-${i}`,
+        value: apiKeys[i]
+      });
+
+      usagePlan.addApiKey(apiKey);
+    }
+
     const api = restApi.root.addResource("api");
     const imageEndpoint = api.addResource("image");
 
-    imageEndpoint.addMethod("GET", new apigateway.LambdaIntegration(getImageHandler));
-    imageEndpoint.addMethod("POST", new apigateway.LambdaIntegration(postImageHandler));
+    imageEndpoint.addMethod("GET", new apigateway.LambdaIntegration(getImageHandler), {
+      apiKeyRequired: true
+    });
+
+    imageEndpoint.addMethod("POST", new apigateway.LambdaIntegration(postImageHandler), {
+      apiKeyRequired: true
+    });
   }
 }
